75bd59679fa1837d1086e3e9526e6cc2ee1de574
Aiden <macuser@MacBook-Air.local>
2024-01-24 13:59:40 -0700

    added js and updated HTML - currently testing for bugs

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,46 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
-  
+console.log(dayjs())
+
+$(() => {
+
+const currentHour = dayjs().format('H')
+
+hourlyColor = () => {
+    $('.time-block').each(function() {
+        const blockHour = parseInt(this.id)
+        $(this).toggleClass('past', blockHour < currentHour)
+        $(this).toggleClass('present', blockHour === currentHour)
+        $(this).toggleClass('future', blockHour > currentHour)
+    })}
+
+textEntry = () => {
+    $('.saveBtn').on('click', function() {
+        const key = $(this).parent().attr('id')
+        const value = $(this).siblings('.description').val()
+        localStorage.setItem(key, value)
+    })}
+
+refreshColor = () => {
+    $('.time-block').each(function() {
+        const blockHour = parseInt(this.id)
+        if (blockHour == currentHour) {
+            $(this).removeClass('past future').addClass('present')
+        } else if (blockHour < currentHour) {
+            $(this).removeClass('future present').addClass('past')
+        } else {
+            $(this).removeClass('past present').addClass('future')
+        }
+    })}
+    $('.time-block').each(function() {
+        const key = $(this).attr('id')
+        const value = localStorage.getItem(key)
+        $(this).children('.description').val(value)
+    })
+
+updateTime = () => {
+    const dateElement = $('#currentDay')
+    const currentDate = dayjs().format('dddd, MMMM D')
+    dateElement.text(currentDate)
+    };setInterval(updateTime, 1000)
+
+    hourlyColor();textEntry();refreshColor()
+})
