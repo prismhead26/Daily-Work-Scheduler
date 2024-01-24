d764e215413a8627d312f3489b746d74be7d1aae
Aiden <macuser@MacBook-Air.local>
2024-01-24 15:20:35 -0700

    Updated js and added comments/App running without errors

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,44 @@
 console.log(dayjs())
-
+// added styling to improve user experience
 $('body').attr('style', 'background: lightslategrey;')
 $('header').attr('style', 'background-color: hsla(50, 33%, 25%, .75);')
 $('.hour').attr('style', 'background-color: orange; border-radius: 15px 0 0 15px;')
-
+// main function
 $(() => {
-
-const currentHour = dayjs().format('H')
-
-hourlyColor = () => {
+    // get previously saved content from local storage
     $('.time-block').each(function() {
-        const blockHour = parseInt(this.id)
-        $(this).toggleClass('past', blockHour < currentHour)
-        $(this).toggleClass('present', blockHour === currentHour)
-        $(this).toggleClass('future', blockHour > currentHour)
-    })}
-
-textEntry = () => {
-    $('.saveBtn').on('click', () => {
-        const key = $(this).parent().attr('id')
-        const value = $(this).siblings('.description').val()
-        localStorage.setItem(key, value)
-    })}
-
-refreshColor = () => {
-    $('.time-block').each(() => {
-        const blockHour = parseInt(this.id)
-        if (blockHour == currentHour) {
-            $(this).removeClass('past future').addClass('present')
-        } else if (blockHour < currentHour) {
-            $(this).removeClass('future present').addClass('past')
-        } else {
-            $(this).removeClass('past present').addClass('future')
-        }
-    })}
-    $('.time-block').each(() => {
         const key = $(this).attr('id')
         const value = localStorage.getItem(key)
         $(this).children('.description').val(value)
     })
-
-updateTime = () => {
+// get current hour from  the Day.js API
+const currentHour = dayjs().format('H')
+// save content/text to the localStorage
+saveContent = () => {
+    $('.saveBtn').on('click',function () {
+        const key = $(this).parent().attr('id')
+        const value = $(this).siblings('.description').val()
+        localStorage.setItem(key, value)
+    })}
+// add color coding in regards to the three different time phases
+colorCode = () => {
+    $('.time-block').each(function() {
+        const blockHour = parseInt(this.id)
+        if (blockHour == currentHour) {
+            $(this).addClass('present')
+        } else if (blockHour < currentHour) {
+            $(this).addClass('past')
+        } else {
+            $(this).addClass('future')
+        }
+    })}
+// using Day.js the time is formatted and attached to DOM element
+// interval set to 1 second to update the date .... tried per hour and basically shows date once per hour
+time = () => {
     const dateElement = $('#currentDay')
     const currentDate = dayjs().format('dddd, MMMM D')
     dateElement.text(currentDate)
-    };setInterval(updateTime, 1000)
-
-    hourlyColor();textEntry();refreshColor()
+    };setInterval(time, 1000)
+    // call the two main functions for text and color
+    ;saveContent();colorCode()
 })
