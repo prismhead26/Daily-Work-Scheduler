b9899d1c1f4cdbc708444a507bf865da0dfabfe2
prismhead26 <prismhead26@gmail.com>
2024-01-24 18:03:43 -0600

    fixed time bug and spacing

--- assets/script.js
+++ assets/script.js
@@ -11,34 +11,37 @@ $(() => {
         const value = localStorage.getItem(key)
         $(this).children('.description').val(value)
     })
-// get current hour from  the Day.js API
-const currentHour = dayjs().format('H')
-// save content/text to the localStorage
-saveContent = () => {
-    $('.saveBtn').on('click',function () {
-        const key = $(this).parent().attr('id')
-        const value = $(this).siblings('.description').val()
-        localStorage.setItem(key, value)
-    })}
-// add color coding in regards to the three different time phases
-colorCode = () => {
-    $('.time-block').each(function() {
-        const blockHour = parseInt(this.id)
-        if (blockHour == currentHour) {
-            $(this).addClass('present')
-        } else if (blockHour < currentHour) {
-            $(this).addClass('past')
-        } else {
-            $(this).addClass('future')
+    // get current hour from  the Day.js API
+    const currentHour = dayjs().format('H')
+    // save content/text to the localStorage
+    saveContent = () => {
+        $('.saveBtn').on('click', function() {
+            const key = $(this).parent().attr('id')
+            const value = $(this).siblings('.description').val()
+            localStorage.setItem(key, value)
+        })}
+    // add color coding in regards to the three different time phases
+    colorCode = () => {
+        $('.time-block').each(function() {
+            const blockHour = parseInt(this.id)
+            if (blockHour == currentHour) {
+                $(this).addClass('present')
+            } else if (blockHour < currentHour) {
+                $(this).addClass('past')
+            } else {
+                $(this).addClass('future')
+            }
+        })}
+    // using Day.js the time is formatted and attached to DOM element
+    // interval set to 1 second to update the date .... tried per hour and basically shows date once per hour
+    time = () => {
+        const dateElement = $('#currentDay')
+        const currentDate = dayjs().format('dddd, MMMM D')
+        dateElement.text(currentDate)
         }
-    })}
-// using Day.js the time is formatted and attached to DOM element
-// interval set to 1 second to update the date .... tried per hour and basically shows date once per hour
-time = () => {
-    const dateElement = $('#currentDay')
-    const currentDate = dayjs().format('dddd, MMMM D')
-    dateElement.text(currentDate)
-    };setInterval(time, 1000)
-    // call the two main functions for text and color
-    ;saveContent();colorCode()
+        setInterval(time, 60000)
+        // call the main functions for text and color and time
+        time()
+        saveContent()
+        colorCode()
 })
